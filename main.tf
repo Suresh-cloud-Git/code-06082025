66e12905666f25ad267eaa80e57d6acf871c2ade
Suresh-cloud-Git <sureshcloud2535@gmail.com>
2025-08-06 16:52:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ variable "rgs" {
   
 }
 resource "azurerm_storage_account" "stgname" {
-    name                     = "storageaccountname11"
+    name                     = "Jaadhu_storage"
   resource_group_name      = "rg-terraformstate2607"
   location                 = "centralindia"
   account_tier             = "Standard"
